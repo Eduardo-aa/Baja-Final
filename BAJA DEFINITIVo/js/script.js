32bbee73859cfcfa5f70b0d8d600f27747b94107
Eduardo.dev <eduardomaia02122006@gmail.com>
2023-12-03 21:07:41 -0300

    on

---
+++ BAJA DEFINITIVo/js/script.js
@@ -0,0 +1,30 @@
+window.addEventListener(("scroll"),()=>{
+    document.querySelector("#").style.marginTop = `${window.scrollY * 0.4}px`
+    document.querySelector("#carro-fundo").style.marginLeft = `${ window.scrollY * 2}px`
+  
+})
+
+function pcar(){
+    img = document.getElementById('img')
+    text = document.getElementById('car-ano')
+    text.innerText = '2021' 
+    img.src = ('imagem/Anne.png')
+}
+function scar(){
+    img = document.getElementById('img')
+    text = document.getElementById('car-ano')
+    text.innerText = '2019' 
+    img.src = ('imagem/Hero.png')
+}
+function tcar(){
+    img = document.getElementById('img')
+    text = document.getElementById('car-ano')
+    text.innerText = '2017' 
+    img.src = ('imagem/Cristine.png')
+}
+function qcar(){
+    img = document.getElementById('img')
+    text = document.getElementById('car-ano')
+    text.innerText = '2015' 
+    img.src = ('imagem/Denso.png')
+}
