70bc2125c978cf0a7e3f032498b6120a00a9cfb1
Eduardo.dev <eduardomaia02122006@gmail.com>
2023-12-19 01:22:05 -0300

    LIGANDOOOO

--- js/script.js
+++ js/script.js
@@ -1,30 +1,47 @@
-window.addEventListener(("scroll"),()=>{
-    document.querySelector("#").style.marginTop = `${window.scrollY * 1}px`
-    document.querySelector("#carro-fundo").style.marginLeft = `${ window.scrollY * 2}px`
-  
-})
+window.addEventListener("scroll", () => {
+  document.querySelector(
+    "#carro-fundo"
+  ).style.marginLeft = `${window.scrollY}px`;
+});
 
-function pcar(){
-    img = document.getElementById('img')
-    text = document.getElementById('car-ano')
-    text.innerText = '2021' 
-    img.src = ('imagem/Anne.png')
+function pcar() {
+  img = document.getElementById("img");
+  text = document.getElementById("car-ano");
+  text2 = document.getElementById("name-car")
+  text2.innerText = "Anne"
+  text.innerText = "2021";
+  img.src = "imagem/Anne.png";
 }
-function scar(){
-    img = document.getElementById('img')
-    text = document.getElementById('car-ano')
-    text.innerText = '2019' 
-    img.src = ('imagem/Hero.png')
+function scar() {
+  img = document.getElementById("img");
+  text = document.getElementById("car-ano");
+  text3 = document.getElementById("name-car")
+  text3.innerText = "Hero"
+  text.innerText = "2019";
+  img.src = "imagem/Hero.png";
 }
-function tcar(){
-    img = document.getElementById('img')
-    text = document.getElementById('car-ano')
-    text.innerText = '2017' 
-    img.src = ('imagem/Cristine.png')
+function tcar() {
+  img = document.getElementById("img");
+  text = document.getElementById("car-ano");
+  text3 = document.getElementById("name-car")
+  text3.innerText = "Cristine"
+  text.innerText = "2017";
+  img.src = "imagem/Cristine.png";
 }
-function qcar(){
-    img = document.getElementById('img')
-    text = document.getElementById('car-ano')
-    text.innerText = '2015' 
-    img.src = ('imagem/Denso.png')
+function qcar() {
+  img = document.getElementById("img");
+  text = document.getElementById("car-ano");
+  text3 = document.getElementById("name-car")
+  text3.innerText = "Denso"
+  text.innerText = "2015";
+  img.src = "imagem/Denso.png";
+}
+document.getElementById("menu-icon").addEventListener("click", Click());
+function Click() {
+  document.querySelector(".navbar-1").style.display = "flex";
+  
+}
+document.getElementById("close-icon").addEventListener("click", Click1());
+function Click1() {
+  document.querySelector(".navbar-1").style.display = "none";
 }
